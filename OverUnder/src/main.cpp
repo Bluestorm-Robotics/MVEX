0fe3d14638bdf876f03c2c29e0f7066919ce90b7
RobbyKidd <kiddr76@mercersburg.edu>
2023-11-07 17:17:41 -0500

    stated the controller and the ports

--- OverUnder/src/main.cpp
+++ OverUnder/src/main.cpp
@@ -15,15 +15,21 @@ vex::brain       Brain;
 
 // define your global instances of motors and other devices here
 
+controller controller1 = controller(controllerType::primary);
+
+motor leftMotor = motor(PORT1);
+motor RightMotor = motor(PORT2);
+motor ClawMotor = motor(PORT8);
+motor ArmMotor = motor(PORT7);
 
 int main() {
 
     Brain.Screen.printAt( 10, 50, "Hello V5" );
    
     while(1) {
-       //if(controller1.ButtonA. pressed)(){
-
-       //}
+       if(controller1.ButtonA.pressing()){
+    ClawMotor.spin(forward);
+}
         // Allow other tasks to run
         this_thread::sleep_for(10);
     }
